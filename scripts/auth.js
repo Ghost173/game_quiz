73918db6acde7e78ab436d236cde4567970af438
ghost173 <jpirakas007@gmail.com>
2020-03-22 18:11:13 +0530

    scope done

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,13 @@
+auth.onAuthStateChanged(user => {
+    // console.log(user)
+    if (user) {
+        console.log('user loged in: ', user)
+    } else {
+        console.log('user loged out')
+    }
+})
+
+
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
  e.preventDefault();
@@ -13,3 +23,28 @@ auth.createUserWithEmailAndPassword(email, password).then(cred => {
     signupForm.reset();
 })
 })
+
+//logout 
+const logout = document.querySelector('#logout');
+logout.addEventListener('click' , (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log('user-signout')
+    })
+})
+
+//login 
+const loginForm = document.querySelector('#login-form')
+loginForm.addEventListener('submit' , (e) => {
+    e.preventDefault();
+
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value; 
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user)
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    })
+}) 
