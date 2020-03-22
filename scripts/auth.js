3e7ccd1cd8941a882e1f202064d103a33ff40f02
ghost173 <jpirakas007@gmail.com>
2020-03-22 17:41:26 +0530

    modify sign up

---
+++ scripts/auth.js
@@ -0,0 +1,15 @@
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+ e.preventDefault();
+
+ const email = signupForm['signup-email'].value;
+ const password = signupForm['signup-password'].value;
+
+//  console.log(email,password)
+auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user)
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+})
+})
